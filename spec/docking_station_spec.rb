884bf9ba455ae2b6e59a258d262b28de4f7cc560
WilliamJGrace <williamjgrace@outlook.com>
2020-02-18 18:11:09 +0000

    added failing test, test is testing if release_bike responds to Docking Station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,11 @@
 require "docking_station"
 
 describe DockingStation do
+
+  describe ".release_bike" do
+    it "exists" do
+      expect(DockingStation).to respond_to(:release_bike)
+    end
+
+  end
 end
